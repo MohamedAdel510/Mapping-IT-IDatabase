6639012004d2c18bf6f8a51dd283e5d7be7b26f4
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2024-09-04 03:36:25 +0300

    with crud operations

--- Mapping IT IDatabase/Entites/Course.cs
+++ Mapping IT IDatabase/Entites/Course.cs
@@ -16,7 +16,7 @@ namespace Mapping_IT_IDatabase.Entites
        public DateTime Duration { get; set; }
        [ForeignKey("Topic")]
        public int? TopicId { get; set; }
-       public Topic Topic { get; set; } //Navigational Property
+       public Topic? Topic { get; set; } //Navigational Property
        public ICollection<StudentCourse> Students { get; set; } = new HashSet<StudentCourse>();
        public ICollection<CourseInstructor> Instructors { get; set; } = new HashSet<CourseInstructor>();
     }
--- Mapping IT IDatabase/Program.cs
+++ Mapping IT IDatabase/Program.cs
@@ -9,7 +9,59 @@ namespace Mapping_IT_IDatabase
         static void Main(string[] args)
         {
             using EnterpriseITIDbContext dbContext = new EnterpriseITIDbContext();
+            #region Insert
+            //Course course = new Course()
+            //{
+            //    Name = "Asp.net",
+            //    Description = "Description",
+            //    Duration = DateTime.Now,
+            //};
+            ////dbContext.Courses.Add(course);
 
+            //Department department = new Department()
+            //{
+            //    Name = "SW",
+            //    HiringDate = DateTime.Now
+            //};
+            ////dbContext.Departments.Add(department);
+
+            //Student student = new Student()
+            //{
+            //    FName = "Mohamed",
+            //    LName = "Adel",
+            //    Age = 23,
+            //    Address = "Cairo",
+            //    DeptId = 1,
+            //};
+            ////dbContext.Students.Add(student);
+            #endregion
+
+            #region Read
+            //var Students = (from S in dbContext.Students.Include(S => S.Department)
+            //                where S.Id == 2
+            //                select S).FirstOrDefault();
+
+            //Console.WriteLine(Students?.FName ?? "NotFound");
+            //Console.WriteLine(Students?.Department.Name ?? "NotFound");
+            #endregion
+
+            #region Update
+            //var student = (from S in dbContext.Students
+            //               where S.Id == 2
+            //               select S).FirstOrDefault();
+            //student.FName = "Ahmed";
+
+            #endregion
+
+            #region Delete
+            //var student = (from S in dbContext.Students
+            //               where S.Id == 8
+            //               select S).FirstOrDefault();
+            
+            //dbContext.Students.Remove(student);
+            #endregion
+
+            dbContext.SaveChanges();
         }
     }
 }
